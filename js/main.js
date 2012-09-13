9f7d9cf3df98da4ca460d3c94997d9c4a833c5e2
THEtheChad <chad.d.elliott@gmail.com>
2012-09-13 14:56:23 -0500

    added speaker template and removed boiler extras

--- js/main.js
+++ js/main.js
@@ -1,16 +1,74 @@
-/* Author:
+// CONFIG
+_.templateSettings.variable = 'm';
 
-*/
-$(function(){
+// DATA
+app = {};
+
+app.speakers = [
+  {
+    name: 'Chad Elliott',
+    avatar: 'https://graph.facebook.com/504872282/picture',
+    bio: 'yo yo yo',
+    social: {
+      twitter: 'http://twitter.com/thethechad',
+      facebook: 'http://facebook.com/thethechad'
+    }
+  },
+  {
+    name: 'Chad Elliott',
+    avatar: 'https://graph.facebook.com/504872282/picture',
+    bio: 'yo yo yo',
+    social: {
+      twitter: 'http://twitter.com/thethechad',
+      facebook: 'http://facebook.com/thethechad'
+    }
+  }
+]
+;//app.speakers
+
+app.templates = {
+  speaker: _.template($('#speaker').html()),
+  social : _.template($('#social').html())
+}
+;//app.templates
+
+// UTILITIES
+
+// IE safe console.log
+function log(){ console && console.log(arguments) }
+
+// INITIALIZE
+initFitText();
+renderSpeakerList();
+
+function initFitText(){
   $('h1').fitText(4, {minFontSize:32, maxFontSize:64});
   $('p') .fitText(5, {minFontSize:16, maxFontSize:32});
-});
+}
 
-//Plug-in usage
-jQuery(function($){
-    $("#about").backgroundScale({
-        imageSelector: "#aboutBg",
-        centerAlign: true,
-        containerPadding: 100
-    });
-});
+function renderSpeakerList(){
+  $('.speakers').append(speakerList);
+}
+
+function speakerList(){
+  return _.map(app.speakers, function(speaker){
+    return app.templates.speaker(speaker);
+  }).join('');
+}
+
+function socialTemplate(list){
+  return _.map(list, function(url, type){
+    var obj = {
+      "type": type.toLowerCase(),
+      "url" : url,
+      "name": capitalizeFirstLetter(type)
+    }
+    ;//var
+
+    return app.templates.social(obj);
+  }).join('');
+}
+
+function capitalizeFirstLetter(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
