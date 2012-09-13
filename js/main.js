1747613175c4b171bd8ef3247089918f2f703b51
Coty D. Beasley <design@beasleycreative.com>
2012-09-13 13:26:32 -0500

    Structural changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@
 
 */
 $(function(){
-  $('h1').fitText(3, {minFontSize:20});
-  $('p') .fitText(4, {minFontSize:14});
+  $('h1').fitText(4, {minFontSize:32, maxFontSize:64});
+  $('p') .fitText(5, {minFontSize:16, maxFontSize:32});
+});
 
+//Plug-in usage
+jQuery(function($){
+    $("#about").backgroundScale({
+        imageSelector: "#aboutBg",
+        centerAlign: true,
+        containerPadding: 100
+    });
 });
