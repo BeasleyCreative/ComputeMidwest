9311a8a5bb644e90317aa6839bbf3b6d3a2622f3
Coty Beasley <design@beasleycreative.com>
2012-09-21 14:41:33 -0500

    Added Speaker, Format Polish

--- js/main.js
+++ js/main.js
@@ -6,12 +6,23 @@ app = {};
 
 app.speakers = [
   {
+    name: 'Scott Chacon',
+    avatar: 'img/speaker/ScottChacon.gif',
+    bio: 'Scott Chacon is CIO of Github, the largest code host in the world. Github recently closed $100m Series A investment, primarily from Andreessen Horowitz – one of the top VC firms in Silicon Valley.',
+    social: {
+      twitter: 'http://twitter.com/chacon',
+      wikipedia: 'http://en.wikipedia.org/wiki/Github',
+      website: 'http://github.com'
+    }
+  },
+  {
     name: 'Jason Hoffman',
     avatar: 'img/speaker/JasonHoffman.gif',
     bio: 'Jason the founder and CTO at Joyent, where he is responsible for overseeing the development and implementation of Joyent’s cloud computing technology.',
     social: {
       twitter: 'http://twitter.com/jasonh',
-      wikipedia: 'http://en.wikipedia.org/wiki/Joyent'
+      wikipedia: 'http://en.wikipedia.org/wiki/Joyent',
+      website: 'http://joyent.com'
     }
   },
   {
@@ -19,15 +30,18 @@ app.speakers = [
     avatar: 'img/speaker/NaithanJones.gif',
     bio: 'Naithan “Nait” Jones founded AgLocal in late 2011 to be a web and mobile platform that will match the supply of independent and family farms that produce meat to the demand of wholesale and retail buyers that seek a direct buying connection to these farms.',
     social: {
-      twitter: 'http://twitter.com/naithanjones'
+      twitter: 'http://twitter.com/naithanjones',
+      website: 'http://aglocal.com'
     }
   },
   {
-    name: 'Scott Chacon',
-    avatar: 'img/speaker/ScottChacon.gif',
-    bio: 'Scott Chacon is CIO of Github, the largest code host in the world. Github recently closed $100m Series A investment, primarily from Andreessen Horowitz – one of the top VC firms in Silicon Valley.',
+    name: 'Ben Milne',
+    avatar: 'img/speaker/BenMilne.gif',
+    bio: 'CEO and Co-Founder of Dwolla, an Iowa & California based company that\'s revolutionizing transactions by proving the nation\'s cheapest payment platform (think 25 cents per transaction). Milne was named in Inc as one of the 30 under 30, highlighting the most promising entrepreneurs.',
     social: {
-      twitter: 'http://twitter.com/chacon'
+      twitter: 'http://twitter.com/bpmilne',
+      wikipedia: 'http://en.wikipedia.org/wiki/Dwolla',
+      website: 'http://dwolla.com'
     }
   }
 ]
